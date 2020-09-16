0ce3e0cc991d36641e9372c7eb86d2c4ae2b30d7
yentim0519 <yentim0519@gmail.com>
2020-09-16 19:03:23 +0800

    background process8

--- application.py
+++ application.py
@@ -15,6 +15,10 @@ application = flask.Flask(__name__)
 # # db = sqlalchemy.orm.scoped_session(sqlalchemy.orm.sessionmaker(bind=engine))  # 這行出問題
 # # Config MySQL
 
+def task_handler(args):
+    
+    # this function takes around 2 minutes to complete
+    return True
 
 @application.route('/')
 def index():
@@ -26,23 +30,17 @@ def result():
     password = flask.request.form['password']
     q = Queue(connection=conn)
     
-    data = q.enqueue(get_course_data, username, password)
-        
-    return flask.render_template('page1.html', tables = [data])
+    job = q.enqueue(task_handler, args)
+    
+    return flask.render_template('page1.html', tables = "processing")
 
 
-# @application.route('/result1', methods=["POST"])
-# def result1():
-#     top_notes = flask.request.form['top_notes']
-#     middle_notes = flask.request.form['middle_notes']
-#     base_notes = flask.request.form['base_notes']
-
-#     cur = mysql.connection.cursor()
-#     cur.execute('''SELECT * FROM perfume_data WHERE top_notes LIKE '%{top_notes}%' and middle_notes LIKE '%{middle_notes}%' and base_notes LIKE '%{base_notes}%' '''.format(top_notes = top_notes, middle_notes = middle_notes, base_notes = base_notes))
-#     brand_datas = cur.fetchall()
-#     return flask.render_template('page2.html', brand_datas = brand_datas)
+@application.route('/result1', methods=["POST"])
+def result1():
+    if job.status == True:
+        return flask.render_template('page1.html', tables = "processing")
 
 
 
-# if __name__ == '__main__':
-#     application.run(host='0.0.0.0')
+if __name__ == '__main__':
+    application.run(host='0.0.0.0')
