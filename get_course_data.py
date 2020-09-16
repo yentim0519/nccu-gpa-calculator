93eafbabb186613907af8fac29944c128515b3a1
yentim0519 <yentim0519@gmail.com>
2020-09-16 18:01:09 +0800

    background process6

--- get_course_data.py
+++ get_course_data.py
@@ -1,30 +1,30 @@
 def get_course_data(username, password):
     target_url = 'https://i.nccu.edu.tw/Home.aspx'
 
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    # chrome_options.add_argument("--headless") #無頭模式
-    # chrome_options.add_argument("--disable-dev-shm-usage")
-    # chrome_options.add_argument("--no-sandbox")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless") #無頭模式
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
 
-    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    # driver.get(target_url)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver.get(target_url)
     
-    # driver.find_element_by_id("captcha_Login1_UserName").send_keys(username)
-    # driver.find_element_by_id("captcha_Login1_Password").send_keys(password)
-    # driver.find_element_by_id("captcha_Login1_ckbLogin").send_keys(Keys.ENTER)
-    # time.sleep(4) #讓javasciprt的東西跑出來，才能進入全人系統 # 這邊應該改得更彈性
-    # driver.find_element_by_id("WidgetContainer730150_Widget730150_HyperLink1").send_keys(Keys.ENTER)
-    # driver.switch_to.window(driver.window_handles[-1])
-    # # print(driver.current_url) # 不知道為什麼一定要print才行
-    # time.sleep(3)
-    # driver.switch_to_alert().dismiss()
-    # # print(driver.current_url)
-    # time.sleep(2)
-    # driver.find_elements_by_xpath("//li[@class='nav2']")[1].click()
+    driver.find_element_by_id("captcha_Login1_UserName").send_keys(username)
+    driver.find_element_by_id("captcha_Login1_Password").send_keys(password)
+    driver.find_element_by_id("captcha_Login1_ckbLogin").send_keys(Keys.ENTER)
+    time.sleep(4) #讓javasciprt的東西跑出來，才能進入全人系統 # 這邊應該改得更彈性
+    driver.find_element_by_id("WidgetContainer730150_Widget730150_HyperLink1").send_keys(Keys.ENTER)
+    driver.switch_to.window(driver.window_handles[-1])
+    # print(driver.current_url) # 不知道為什麼一定要print才行
+    time.sleep(3)
+    driver.switch_to_alert().dismiss()
+    # print(driver.current_url)
+    time.sleep(2)
+    driver.find_elements_by_xpath("//li[@class='nav2']")[1].click()
 
-    # html = driver.page_source
-    # soup = BeautifulSoup(html)
+    html = driver.page_source
+    soup = BeautifulSoup(html)
 
     # data = []
     # all_table = soup.find_all("table")
@@ -39,4 +39,4 @@ def get_course_data(username, password):
     #         table_data.append(tr_data)
     #     data.append(table_data)
 
-    return target_url
+    return soup
