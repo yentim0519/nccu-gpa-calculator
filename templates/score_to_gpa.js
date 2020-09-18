66d42218ee5d0ab4a205cb32a5d0f1dfddfcc301
yentim0519 <yentim0519@gmail.com>
2020-09-18 13:30:27 +0800

    page1-65

--- templates/score_to_gpa.js
+++ templates/score_to_gpa.js
@@ -1,53 +1,39 @@
 function score_to_gpa_4point3() {
     // var score_interval = [90, 85, 80, 77, 73, 70, 67, 63, 60, 50, 0];
-
     // if (score >= score_interval[0]){
     //     return 4.3; 
     // }
-        
-    
     // else if (score >= score_interval[1] & score < score_interval[0]){
     //     return 4.0;
     // }
-        
-
     // else if (score >= score_interval[2] & score < score_interval[1]){
     //     return 3.7;
     // }
-
     // else if (score >= score_interval[3] & score < score_interval[2]){
     //     return 3.3;
     // }
-
     // else if (score >= score_interval[4] & score < score_interval[3]){
     //     return 3.0;
     // }
-
     // else if (score >= score_interval[5] & score < score_interval[4]){
     //     return 2.7;
     // }
-
     // else if (score >= score_interval[6] & score < score_interval[5]){
     //     return 2.3;
     // }
-
     // else if (score >= score_interval[7] & score < score_interval[6]){
     //     return 2.0;
     // }
-
     // else if (score >= score_interval[8] & score < score_interval[7]){
     //     return 1.7;
     // }
-
     // else if (score >= score_interval[9] & score < score_interval[8]){
     //     return 1.0;
     // }
-
     // else if (score >= score_interval[10] & score < score_interval[9]){
     //     return 0.0;
     // }
     // return score * 2;
-    alert("it works!")
-    
-}
+    alert("it works!");
 
+}
