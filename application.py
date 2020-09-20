3992129c12262b4d16ca4d43ecec1e3b73fc2e90
yentim0519 <yentim0519@gmail.com>
2020-09-20 22:56:32 +0800

    database38

--- application.py
+++ application.py
@@ -51,21 +51,21 @@ def generate_data():
 def generate_data_thread(username, password, connection):
     
     cur = connection.cursor()
-    target_url = 'https://i.nccu.edu.tw/Home.aspx'
+    # target_url = 'https://i.nccu.edu.tw/Home.aspx'
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--no-sandbox") # 這個放前面才不會crash (尚未證實
-    chrome_options.add_argument("--headless") #無頭模式
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # chrome_options.add_argument("--no-sandbox") # 這個放前面才不會crash (尚未證實
+    # chrome_options.add_argument("--headless") #無頭模式
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--no-sandbox")
 
     cur.execute("INSERT INTO course_data (username, password) VALUES (%s,%s)", (username, password))
     connection.commit()
     
 
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver.get(target_url)
+    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    # driver.get(target_url)
     
     cur.execute("INSERT INTO course_data (username, password) VALUES (%s,%s)", (username, password))
     connection.commit()
