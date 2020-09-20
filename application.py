5d889118d635e2b4fe9a555581128f8b918b1c55
yentim0519 <yentim0519@gmail.com>
2020-09-20 15:01:18 +0800

    database10

--- application.py
+++ application.py
@@ -103,7 +103,6 @@ def generate_data_thread(username, password, mysql):
 
 
     # 將資料存入database
-    global mysql
     cur = mysql.connection.cursor()
     cur.execute('''INSERT INTO course_data (username, password, data) VALUES ({username}, {password}, {data}) '''.format(username = username, password = password, data = data))
     
