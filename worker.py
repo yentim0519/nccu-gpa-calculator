c429617cb2a47a515797e8bb84640ccca25c230a
yentim0519 <yentim0519@gmail.com>
2020-09-24 12:54:34 +0800

    session31

--- worker.py
+++ worker.py
@@ -5,7 +5,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('redis://redistogo:bec8e321794ffa93fa51afcc703334e3@pike.redistogo.com:11600/', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
 
