9b8f0207b5aad91fa88ff5f910b18d1e0f3304e0
yentim0519 <yentim0519@gmail.com>
2020-12-25 15:55:26 +0800

    change to upload files

--- application.py
+++ application.py
@@ -1,19 +1,15 @@
 import flask
-from flask import request, jsonify,session
+from flask import request, jsonify, session, redirect, url_for
 from flask_session import Session
 import os
 from bs4 import BeautifulSoup
-import selenium
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select 
-from selenium.webdriver.common.keys import Keys
-import selenium.webdriver.support.ui as ui
 import time
 from rq import Queue
 from rq.job import Job
 from rq.registry import StartedJobRegistry
 from worker import conn
 import json 
+from werkzeug.utils import secure_filename
 
 
 application = flask.Flask(__name__)
@@ -23,85 +19,59 @@ q = Queue(connection=conn)
 application.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
+UPLOAD_FOLDER = '/tmp' #folder route of uploading file
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif']) # limitation of upload file format
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
+
 
 @application.route('/', methods=["GET", "POST"])
 def index():
     if request.method == 'POST':
-        username = flask.request.form['username']
-        password = flask.request.form['password']
-        # grad_or_not = flask.request.form['password']
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
 
-        task = q.enqueue(generate_data_thread, args=(username,password))
+        task = q.enqueue(generate_data_thread, args=(file_path))
         task_id = task.get_id()
-
         return flask.render_template('loading_page.html', task_id=task_id)
+    else:
+        return flask.render_template('index.html')
 
-    return flask.render_template('index.html')
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 
 
 
 # get data
-def generate_data_thread(username, password):
-
-    target_url = 'https://i.nccu.edu.tw/Home.aspx'
-
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--no-sandbox") # 這個放前面才不會crash (尚未證實
-    chrome_options.add_argument("--headless") #無頭模式
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+def generate_data_thread(html_file_path):
     
-    try:
-        driver.get(target_url)
+    with open(html_file_path, encoding="utf-8") as f:
+        data0 = f.read()
+        soup = BeautifulSoup(data0, 'html.parser')
 
-        wait = ui.WebDriverWait(driver,100) # 100秒內，每500毫秒掃描一次
-        wait.until(lambda driver: driver.find_element_by_id("captcha_Login1_UserName"))
-
+    data = [] #要存進database
+    all_table = soup.find_all("table")
+    for table in all_table[5:]:
         
-        # # 這邊要try catch一下
-        driver.find_element_by_id("captcha_Login1_UserName").send_keys(username)
-        driver.find_element_by_id("captcha_Login1_Password").send_keys(password)
-        driver.find_element_by_id("captcha_Login1_ckbLogin").send_keys(Keys.ENTER)
-
-
-        wait.until(lambda driver: driver.find_element_by_id("WidgetContainer730150_Widget730150_HyperLink1"))
-        driver.find_element_by_id("WidgetContainer730150_Widget730150_HyperLink1").send_keys(Keys.ENTER)
-
-        driver.switch_to.window(driver.window_handles[-1])
-        time.sleep(3) # 改成wait until
-        driver.switch_to_alert().dismiss()
-        # print(driver.current_url)
-        wait.until(lambda driver: driver.find_elements_by_xpath("//li[@class='nav2']")[1])
-        driver.find_elements_by_xpath("//li[@class='nav2']")[1].click()
-
-        html = driver.page_source
-        soup = BeautifulSoup(html)
-        print(soup)
-
-        data = [] #要存進database
-        all_table = soup.find_all("table")
-        for table in all_table[5:]:
+        table_data = []
+        all_tr = table.find_all("tr")
+        for tr in all_tr[2:]:
             
-            table_data = []
-            all_tr = table.find_all("tr")
-            for tr in all_tr[2:]:
-                
-                tr_data = []
-                all_td = tr.find_all("td")
-                for td in all_td:
-                    tr_data.append(td.string)
+            tr_data = []
+            all_td = tr.find_all("td")
+            for td in all_td:
+                tr_data.append(td.string)
 
-                table_data.append(tr_data)
-            data.append(table_data)
-        driver.close()
-    except selenium.common.exceptions.WebDriverException:
-        driver.close()
-        return flask.render_template('error_page.html')
-
+            table_data.append(tr_data)
+        data.append(table_data)
+    
     return json.dumps(data) # 這邊要轉成json檔，result才能decode回來
 
     
@@ -135,7 +105,6 @@ def thread_status():
 
 @application.route('/result', methods=["POST", "GET"]) 
 def result():  
-    
     task_id = flask.request.form['task_id']
     print("result", task_id)
     task = Job.fetch(task_id, connection=conn)
@@ -155,7 +124,6 @@ if __name__ == '__main__':
 
 
 # Must:
-# 密碼會加密（不會讓人看到
 # handle result要按很多次才能跑出來
 # 手機版的可以把不重要的資訊拿掉，讓table變小
 
@@ -163,8 +131,6 @@ if __name__ == '__main__':
 # Nice to have:
 # handle 一個學期全選
 # handle 在generate時refresh
-# handle 晚上學校系統維修的狀況
 # 符合各平台版本 --> 在手機上要大一點
 # 紀錄使用人次
-# 給延畢生
 # 中間弄個進度條，讓人知道等待時間
