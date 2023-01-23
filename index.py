378323029bf82b8df23a38ffc02578f5f66905a1
yentim0519 <yentim0519@gmail.com>
2023-01-23 21:51:57 +0800

    move allowed_file() into index() to avoid error

--- index.py
+++ index.py
@@ -21,7 +21,12 @@ app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024  # 1MB
 
 @app.route('/', methods=["GET", "POST"])
 def index(): 
-    # return 'Hello World!'
+    # Make sure the file is in the correct formats (ex. .html, .htm)
+    def allowed_file(filename):
+        return '.' in filename and \
+            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    
+    
     try:
         if request.method == 'POST':
             
@@ -31,8 +36,7 @@ def index():
                     flash("No file upload!")
                     return redirect(request.url)
 
-                # if file and allowed_file(file.filename): # allowed_file會根據allow extension限制
-                if file:
+                if file and allowed_file(file.filename): # allowed_file會根據allow extension限制
                     filename = secure_filename(file.filename) # never trust user input
                     if file.filename == '':
                         flash('No selected file')
@@ -50,36 +54,31 @@ def index():
                 # 將data存到一個list中
                 data = [] 
                 all_table = soup.find_all("table")
+
                 for table in all_table[5:]:
-                    
                     table_data = []
                     all_tr = table.find_all("tr")
+
                     for tr in all_tr[2:]:
                         
                         tr_data = []
                         all_td = tr.find_all("td")
+
                         for td in all_td:
                             tr_data.append(td.string)
 
                         table_data.append(tr_data)
+
                     data.append(table_data)
                 
                 return flask.render_template('page1.html', data_all = data) 
         else:
             return flask.render_template('index.html')
+            
     except RequestEntityTooLarge:
         flash("The file is too large! The limit is 1MB.")
         return redirect(request.url) 
         
-
-
-
-# Make sure the file is in the correct formats (ex. .html, .htm)
-# def allowed_file(filename):
-#     return '.' in filename and \
-#         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-
 
 if __name__ == '__main__':
     # app.debug=True
